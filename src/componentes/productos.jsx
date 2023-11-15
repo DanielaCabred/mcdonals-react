7c8a4733388a0f6fd501bcfe1944848770930fac
Daniela Cabred <danielacabred@gmail.com>
2023-11-15 20:54:06 -0300

    cargaprodutos

---
+++ src/componentes/productos.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const productos = () => {
+    let productosLista= [
+        {
+            id:1, nombre: CuartoDeLibra, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXTUDbN/200/200/original?country=ar", precio: $1500, categoria: hamburguesa
+        },
+        {
+            id: 2, nombre: McNifica, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXXaUUP/200/200/original?country=ar", precio: $1800
+        },
+        {
+            id: 3, nombre: TripleHamburguesaconQueso, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXsCqnz/200/200/original?country=ar", precio: $1200, categoria: hambueguesa
+        },
+        {
+            id: 4, nombre: papasGrandes, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcXXQgnB/200/200/original?country=ar", precio: $1000, categoria: papas
+        },
+        {
+            id:5, nombre: coca, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcX6zxgK/200/200/original?country=ar", precio: $1000, categoria: bebidas
+        }
+
+    ]
+
+  return (
+    <div>productos</div>
+  )
+}
+
+export default productos
