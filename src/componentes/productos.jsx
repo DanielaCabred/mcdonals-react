eafd4c6c1e4b8e30c908409ad2abb8f9784fa326
Daniela Cabred <danielacabred@gmail.com>
2023-11-15 20:59:46 -0300

    correccion

--- src/componentes/productos.jsx
+++ src/componentes/productos.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 const productos = () => {
     let productosLista= [
         {
-            id:1, nombre: CuartoDeLibra, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXTUDbN/200/200/original?country=ar", precio: $1500, categoria: hamburguesa
+            id:1, nombre: "CuartoDeLibra", img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXTUDbN/200/200/original?country=ar", precio: 1500, categoria: "hamburguesas"
         },
         {
-            id: 2, nombre: McNifica, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXXaUUP/200/200/original?country=ar", precio: $1800
+            id:2, nombre: "McNifica", img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXXaUUP/200/200/original?country=ar", precio: 1800, categoria: "hambueguesas"
         },
         {
-            id: 3, nombre: TripleHamburguesaconQueso, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXsCqnz/200/200/original?country=ar", precio: $1200, categoria: hambueguesa
+            id:3, nombre: "TripleHamburguesaconQueso", img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqXsCqnz/200/200/original?country=ar", precio: 1200, categoria: "hambueguesas"
         },
         {
-            id: 4, nombre: papasGrandes, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcXXQgnB/200/200/original?country=ar", precio: $1000, categoria: papas
+            id:4, nombre: "papasGrandes", img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcXXQgnB/200/200/original?country=ar", precio: 1000, categoria: "papas"
         },
         {
-            id:5, nombre: coca, img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcX6zxgK/200/200/original?country=ar", precio: $1000, categoria: bebidas
+            id:5, nombre: "coca", img: "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kcX6zxgK/200/200/original?country=ar", precio: 1000, categoria: "bebidas"
         }
 
     ]
 
-  return (
+    return (
     <div>productos</div>
-  )
+    )
 }
 
 export default productos
